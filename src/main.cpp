dc757068439a916cf912c5b8a3da9c54c921012e
Avinash Ahilesan <avinash.ahilesan00@gmail.com>
2024-07-02 08:25:21 -0400

    Add some lexer code, file structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "lexer.h"
+
 using namespace std;
 
 int main() {
-    cout << "Hello World";
+    std::ifstream source_file ("../input/source.hb");
+    std::string fileString;
+
+    if (source_file.is_open()) {
+        std::string nextLine;
+        while (source_file) {
+         std::getline(source_file, nextLine);
+         fileString.append(nextLine);
+         fileString.append("\n");
+        }
+    }
+    std::cout << fileString;
     return 0;
 }
+

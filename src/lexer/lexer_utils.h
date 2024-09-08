02bd2dcaa79f0c6f1bdb54548c098d1dabec5219
Avinash Ahilesan <avi2002@students.cs.ubc.ca>
2024-09-07 21:23:04 -0400

    Fix issues in lexer, some tokens can now be lexed correctly

--- src/lexer/lexer_utils.h
+++ src/lexer/lexer_utils.h
@@ -7,7 +7,8 @@ class LexerUtils {
 
     public:
         static bool is_digit(char c);
-        static bool is_char(char c);
+        static bool is_alphabetical(char c);
+        static bool is_underscore(char c);
     private:
 
 };

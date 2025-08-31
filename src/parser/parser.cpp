f0b7f3542c3576ddaea9b953dd8ad28bbb2af0ef
Avinash Ahilesan <avi2002@students.cs.ubc.ca>
2025-08-31 23:54:28 +0300

    Fix bug in parser for mixed multipl/division and addition/subtraction. Add tests and other functionality

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -1,207 +1,363 @@
 #include "../parser/parser.h"
 #include <iostream>
 
-Parser::Parser(std::vector<Token> token_list) {
+Parser::Parser(std::vector<Token> token_list)
+{
     this->word_list = token_list;
 }
 
-void printTokenType(TokenType token_type) {
-        switch (token_type)
-            {
-            case TokenType::TYPE_INTEGER:
-                /* code */
-                std::cout<< "type integer \n";
-                break;
-            case TokenType::VAL_INTEGER:
-                std::cout << "val integer: \n";
-                break;
-            case TokenType::IDENTIFIER:
-                /* code */
-                std::cout<< "identifier: \n";
-                break;
-            case TokenType::EQUALS:
-                /* code */
-                std::cout<< "equals \n";
-                break;
-            case TokenType::VAL_STRING:
-                std::cout << "string \n";
-                break;
-            default:
-                std::cout<< "other token \n";
-                break;
-            }
+void printTokenType(TokenType token_type)
+{
+    switch (token_type)
+    {
+    case TokenType::TYPE_INTEGER:
+        /* code */
+        std::cout << "type integer \n";
+        break;
+    case TokenType::VAL_INTEGER:
+        std::cout << "val integer: \n";
+        break;
+    case TokenType::IDENTIFIER:
+        /* code */
+        std::cout << "identifier: \n";
+        break;
+    case TokenType::EQUALS:
+        /* code */
+        std::cout << "equals \n";
+        break;
+    case TokenType::VAL_STRING:
+        std::cout << "string \n";
+        break;
+    default:
+        std::cout << "other token \n";
+        break;
+    }
 }
 
-void Parser::next_word() {
-    if (++curr_index >= word_list.size()) {
-        curr_word =  NULL; // signify end of list
+void Parser::next_word()
+{
+    if (++curr_index >= word_list.size())
+    {
+        curr_word = NULL; // signify end of list
     }
     curr_word = &word_list[curr_index];
 }
 
-bool Parser::parse() {
+void Parser::parse()
+{
     // Entry point Goal -> Expr
+    std::cout << "IN PARSE";
     this->next_word();
-
-    if (is_one_of(this->curr_word, TokenType::TYPE_INTEGER)) {
-        if (variable_decl()) {
-            if (is_one_of(this->curr_word, TokenType::END_OF_FILE)) {
-                return true;
-            } else {
-                std::cout << "Last character is not end of file";
+    if (is_one_of(this->curr_word, TokenType::TYPE_INTEGER))
+    {
+        if (variable_decl())
+        {
+            if (!is_one_of(this->curr_word, TokenType::END_OF_FILE))
+            {
+                throw std::runtime_error("Last character is not end of file");
             }
         }
+        return;
     }
-    else if (expr()) {
-        if (is_one_of(this->curr_word, TokenType::END_OF_FILE)) {
-            return true;
+    else
+    {
+        auto e = expr();
+        if (!is_one_of(this->curr_word, TokenType::END_OF_FILE))
+        {
+            throw std::runtime_error("Last character is not end of file");
         }
-        else {
-            std::cout << "Last character is not end of file";
-        }
-    } 
-    std::cout << "Not an expression";
-    return false;
+        g.expr = e;
+        return;
+    }
+    throw std::runtime_error("Not an expression");
 }
 
 // Expr -> Term Expr'
-bool Parser::expr() {
-    if (term()) {
-        if (expr_prime()) {
-            return true;
-        }
-        std::cout << "Not an Expr'";
-        return false;
-    }
+std::variant<Factor *, Expr *> Parser::expr()
+{
+    // 5 * 5  
+    std::cout << "IN EXPR";
+    auto left = term();
+    return expr_prime(left);
     std::cout << "Not a term";
-    return false;
 }
 
 // Term -> Factor Term'
-bool Parser::term() {
-    if (factor()) {
-        return term_prime();
-    }
-    std::cout << "Not a factor";
-    return false;
+std::variant<Factor *, Expr *> Parser::term()
+{
+    std::cout << "IN TERM";
+    // Expr
+    auto lhs = factor();
+    return term_prime(lhs);
 }
 
 // Term' -> x Factor Term'
 //       -> / Factor Term'
 //       -> empty
-bool Parser::term_prime() {
-    if (is_one_of(this->curr_word, TokenType::MULTIPLY, TokenType::DIVIDE)) {
+std::variant<Factor*, Expr*> Parser::term_prime(std::variant<Factor *, Expr *> lhs)
+{
+    std::cout << "IN TERM'";
+    if (is_one_of(this->curr_word, TokenType::MULTIPLY, TokenType::DIVIDE))
+    {
+        Expr* expr = new Expr();
+        expr->lhs = lhs;
+        if (is_one_of(this->curr_word, TokenType::MULTIPLY))
+        {
+            expr->op = Operator::MULTIPLY;
+        }
+        if (is_one_of(this->curr_word, TokenType::DIVIDE))
+        {
+            expr->op = Operator::DIVIDE;
+        }
         this->next_word();
-        if (factor()) {
-            return term_prime();
-        }
-        else {
-            std::cout << "NOT A FACTOR";
-            return false;
-        }
-    } 
-    if (is_one_of(this->curr_word, TokenType::PLUS, TokenType::MINUS, TokenType::CLOSE_ROUND_BRACKET, TokenType::END_OF_FILE)) {
+        std::cout << "next word";
+        printTokenType(this->curr_word->token_type);
+        std::variant<Factor *, Expr *> rhs = factor();
+        expr->rhs = rhs;
+        return term_prime(expr);
+    }
+    if (is_one_of(this->curr_word, TokenType::PLUS, TokenType::MINUS, TokenType::CLOSE_ROUND_BRACKET, TokenType::END_OF_FILE))
+    {
         // we can eat an epsilon if the term that follows is )
         // FOLLOW(term') = + - )
-        return true;
+        std::cout << "term': true from follow";
+        return lhs;
     }
-    std::cout << "Not a term prime";
-    return false;
+    throw std::runtime_error("Not a term prime");
 }
 
 //
-bool Parser::expr_prime() {
-    if (is_one_of(this->curr_word, TokenType::PLUS, TokenType::MINUS)) {
+std::variant<Factor *, Expr *> Parser::expr_prime(std::variant<Factor *, Expr *> lhs)
+{
+    std::cout << "IN EXPR'";
+    if (is_one_of(this->curr_word, TokenType::PLUS, TokenType::MINUS))
+    {
+        Expr *expr = new Expr();
+        expr->lhs = lhs;
+        if (is_one_of(this->curr_word, TokenType::PLUS))
+        {
+            expr->op = Operator::ADD;
+        }
+        if (is_one_of(this->curr_word, TokenType::MINUS))
+        {
+            expr->op = Operator::SUBTRACT;
+        }
+
         this->next_word();
-        if (factor()) {
-            return term_prime();
-        }
-        else {
-            std::cout << "NOT A FACTOR";
-            return false;
-        }
+
+        std::variant<Factor *, Expr *> rhs = term();
+        expr->rhs = rhs;
+        return expr_prime(expr);
     }
-    if (is_one_of(this->curr_word, TokenType::CLOSE_ROUND_BRACKET, TokenType::END_OF_FILE)) {
-        return true;
+    if (is_one_of(this->curr_word, TokenType::CLOSE_ROUND_BRACKET, TokenType::END_OF_FILE))
+    {
+        return lhs;
     }
-    std::cout << "not an expression prime";
-    return false;
+    throw std::runtime_error("not an expression prime");
 }
 
-bool Parser::factor() {
-    if (is_one_of(this->curr_word, TokenType::OPEN_ROUND_BRACKET)) {
+std::variant<Factor *, Expr *> Parser::factor()
+{
+    std::cout << "IN FACTOR";
+    if (is_one_of(this->curr_word, TokenType::OPEN_ROUND_BRACKET))
+    {
         this->next_word();
-        if (expr())
-            if (is_one_of(this->curr_word, TokenType::CLOSE_ROUND_BRACKET)) {
-                this->next_word();
-                return true;
-            }
-        else 
-            return false;
+        std::cout << "creating expr for factor, brackets detected";
+        auto sub_expr = expr();
+        if (is_one_of(this->curr_word, TokenType::CLOSE_ROUND_BRACKET))
+        {
+            this->next_word();
+            std::cout << "returning expr";
+            return sub_expr;
+        }
+        throw std::runtime_error("Unmatched opening bracket");
     }
-    else if (is_one_of(this->curr_word, TokenType::VAL_INTEGER, TokenType::IDENTIFIER)) {
+    else if (is_one_of(this->curr_word, TokenType::VAL_INTEGER, TokenType::IDENTIFIER))
+    {
+        // create correct node type
+        Factor *factor = new Factor();
+        if (is_one_of(this->curr_word, TokenType::VAL_INTEGER))
+        {
+            std::cout << "processing val int";
+            Num *num = new Num();
+            num->num = std::stoi(this->curr_word->token_value);
+            factor->variant = num;
+        } 
+        else if (is_one_of(this->curr_word, TokenType::IDENTIFIER)) {
+            std::cout << "processing identifier";
+            Name *name = new Name();
+            name->name = new std::string(this->curr_word->token_value);
+            factor->variant = name;
+        }
         this->next_word();
-        return true;
+        return factor;
     }
-    return false;
+    throw std::runtime_error("Not a factor");
 }
 
-bool Parser::identifier_and_equals() {
+bool Parser::identifier_and_equals()
+{
     this->next_word();
-    if (is_one_of(this->curr_word, TokenType::IDENTIFIER)) {
+    if (is_one_of(this->curr_word, TokenType::IDENTIFIER))
+    {
         this->next_word();
-        if (is_one_of(this->curr_word, TokenType::EQUALS)) {
-            return true;          
-        } else {
+        if (is_one_of(this->curr_word, TokenType::EQUALS))
+        {
+            return true;
+        }
+        else
+        {
             std::cout << "expecting equals, found something else";
         }
-    } else {
+    }
+    else
+    {
         std::cout << "expecting identifier, found something else: ";
         printTokenType(this->curr_word->token_type);
     }
-    
+
     return false;
 }
 
-bool Parser::variable_decl() {
-    if (is_one_of(this->curr_word, TokenType::TYPE_INTEGER)) {
-        if (identifier_and_equals()) {
+bool Parser::variable_decl()
+{
+    if (is_one_of(this->curr_word, TokenType::TYPE_INTEGER))
+    {
+        if (identifier_and_equals())
+        {
             this->next_word();
-            if (is_one_of(this->curr_word, TokenType::VAL_INTEGER)) {
+            if (is_one_of(this->curr_word, TokenType::VAL_INTEGER))
+            {
                 return true;
-            } else {
+            }
+            else
+            {
                 std::cout << "TYPE MISMATCH: INT ASSIGNMENT MISMATCH";
             }
         }
-    } else if (is_one_of(this->curr_word, TokenType::TYPE_STRING)) {
-        if (identifier_and_equals()) {
+    }
+    else if (is_one_of(this->curr_word, TokenType::TYPE_STRING))
+    {
+        if (identifier_and_equals())
+        {
             this->next_word();
-            if (is_one_of(this->curr_word, TokenType::VAL_STRING)) {
+            if (is_one_of(this->curr_word, TokenType::VAL_STRING))
+            {
                 return true;
-            } else {
-                std::cout << "TYPE MISMATCH: INT ASSIGNMENT MISMATCH";
             }
-        } 
-    } else if (is_one_of(this->curr_word, TokenType::TYPE_CHAR)) {
-        if (identifier_and_equals()) {
-            this->next_word();
-            if (is_one_of(this->curr_word, TokenType::VAL_CHAR)) {
-                return true;
-            } else {
+            else
+            {
                 std::cout << "TYPE MISMATCH: INT ASSIGNMENT MISMATCH";
             }
         }
-    } else if (is_one_of(this->curr_word, TokenType::TYPE_FLOAT)) {
-        if (identifier_and_equals()) {
+    }
+    else if (is_one_of(this->curr_word, TokenType::TYPE_CHAR))
+    {
+        if (identifier_and_equals())
+        {
             this->next_word();
-            if (is_one_of(this->curr_word, TokenType::VAL_FLOAT)) {
+            if (is_one_of(this->curr_word, TokenType::VAL_CHAR))
+            {
                 return true;
-            } else {
+            }
+            else
+            {
+                std::cout << "TYPE MISMATCH: INT ASSIGNMENT MISMATCH";
+            }
+        }
+    }
+    else if (is_one_of(this->curr_word, TokenType::TYPE_FLOAT))
+    {
+        if (identifier_and_equals())
+        {
+            this->next_word();
+            if (is_one_of(this->curr_word, TokenType::VAL_FLOAT))
+            {
+                return true;
+            }
+            else
+            {
                 std::cout << "TYPE MISMATCH: INT ASSIGNMENT MISMATCH";
             }
         }
     }
 }
 
+template <typename... Ts>
+struct Overload : Ts...
+{
+    using Ts::operator()...;
+};
+template <class... Ts>
+Overload(Ts...) -> Overload<Ts...>;
 
+struct Visitor
+{
+    void operator()(Expr *e)
+    {
+    }
 
+    void operator()(Factor *f)
+    {
+    }
+};
+
+std::string Parser::getTreeString()
+{
+
+    std::function<std::string(std::variant<Factor *, Expr *>)> expr_to_string;
+
+    std::function<std::string(Operator)> op_to_string;
+
+    op_to_string = [&](Operator op) -> std::string {
+        if (op == Operator::ADD) return "+";
+        if (op == Operator::DIVIDE) return "/";
+        if (op == Operator::MULTIPLY) return "*";
+        if (op == Operator::SUBTRACT) return "-";
+        return "B";
+    };
+
+    std::function<std::string(FactorVariant)> factor_to_string;
+
+    factor_to_string = [&](FactorVariant factor) -> std::string
+    {
+        return std::visit(Overload{[&](Name *e) -> std::string
+                            {
+                                return *e->name;
+                            },
+                            [&](Num *f) -> std::string
+                            {
+                                return std::to_string(f->num);
+                            }},
+                   factor);
+    };
+
+    expr_to_string = [&](std::variant<Factor *, Expr *> node) -> std::string
+    {
+        return std::visit(Overload{[&](Expr *e) -> std::string
+                            {
+                                std::cout << "EXPR TO STRING";
+                                std::string lhs = expr_to_string(e->lhs);
+                                std::string rhs = expr_to_string(e->rhs);
+                                return "(" + op_to_string(e->op) + " " + lhs + " " + rhs + ")";
+                            },
+                            [&](Factor *f) -> std::string
+                            {
+                                std::cout << "FACTOR TO STRING";
+                                if (f != NULL) {
+                                    return factor_to_string(f->variant);
+                                }
+                                return "F";
+                            }},
+                   node);
+    };
+
+    std::cout << "TO STRING";
+    return expr_to_string(g.expr);
+}
+
+void Parser::printTree()
+{
+    std::cout << "\n";
+    std::cout << getTreeString();
+}
